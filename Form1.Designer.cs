73257c8eafdeef43d71fbb4eb6296b65281fdf6a
jeongseoanjeong <seojin123456@naver.com>
2020-06-23 12:45:40 +0900

    adding files

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace OMR_scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.mPictureBox = new System.Windows.Forms.PictureBox();
+            this.answerbox = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.answer = new System.Windows.Forms.RichTextBox();
+            this.point_ = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(293, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "파일 불러오기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(761, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "유의 사항: 이 프로그램은 베타 버전이므로(다른 OMR을 넣게 되면 작동을 안 할 수 있습니다.)\r\n 반드시 동봉한 OMR을 프린트하여 답안을 작" +
+    "성하십시오.\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // mPictureBox
+            // 
+            this.mPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mPictureBox.Location = new System.Drawing.Point(12, 304);
+            this.mPictureBox.Name = "mPictureBox";
+            this.mPictureBox.Size = new System.Drawing.Size(1000, 400);
+            this.mPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mPictureBox.TabIndex = 2;
+            this.mPictureBox.TabStop = false;
+            // 
+            // answerbox
+            // 
+            this.answerbox.Location = new System.Drawing.Point(943, 12);
+            this.answerbox.Name = "answerbox";
+            this.answerbox.Size = new System.Drawing.Size(227, 243);
+            this.answerbox.TabIndex = 3;
+            this.answerbox.Text = "이곳에 답안이 표시됩니다.";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(789, 261);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(442, 22);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "점수를 동일시하여 채점(ex:4문제면 1문제당 25점)\r\n";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // answer
+            // 
+            this.answer.Location = new System.Drawing.Point(1176, 12);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(227, 243);
+            this.answer.TabIndex = 5;
+            this.answer.Text = "정답을 입력하십시오.(\",\"로 구분)";
+            this.answer.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // point_
+            // 
+            this.point_.Location = new System.Drawing.Point(1400, 12);
+            this.point_.Name = "point_";
+            this.point_.Size = new System.Drawing.Size(227, 243);
+            this.point_.TabIndex = 6;
+            this.point_.Text = "점수를 입력하십시오.(\",\"로 구분)";
+            this.point_.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(493, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 63);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "채점";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(651, 188);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(162, 28);
+            this.result.TabIndex = 8;
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1639, 1050);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.point_);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.answerbox);
+            this.Controls.Add(this.mPictureBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "OMR master";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox mPictureBox;
+        private System.Windows.Forms.RichTextBox answerbox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox answer;
+        private System.Windows.Forms.RichTextBox point_;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OMR_scanner
+{
+    public partial class Form1 : Form
+    {
+        public   double finalpoint;
+        public int[] ans2 = new int[400];
+        public int[] omr_answer = new int[400];
+        public int[] ans = new int[400];
+        public int num = 40;
+        public int num3 = 1;
+        public int[] injeok = new int[5];
+        public int b = 0;
+        public int p = 0;
+        public Point startpos1 = new Point(1323, 483);
+        public Point Startpos2 = new Point(1663, 483);
+        public int plusvalX = 40, plusvalY = 70;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShowFileOpenDialog();
+        }
+
+
+        /// <summary>
+        /// 그림파일오픈창을 로드후 해당 파일의 FullPath를 가져온다.
+        /// </summary>
+        /// <returns>파일의 FullPath 파일이 없거나 선택을 안할경우 ""를 리턴</returns>
+        public string ShowFileOpenDialog()
+        {
+            //파일오픈창 생성 및 설정
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "OMR 스캔된 파일 가져오기...";
+            ofd.Filter = "그림 파일 (*.jpg, *.gif, *.bmp) | *.jpg; *.gif; *.bmp; | 모든 파일 (*.*) | *.*";
+
+            //파일 오픈창 로드
+            DialogResult dr = ofd.ShowDialog();
+
+            //OK버튼 클릭시
+            if (dr == DialogResult.OK)
+            {
+                //File명과 확장자를 가지고 온다.
+                string fileName = ofd.SafeFileName;
+                //File경로와 File명을 모두 가지고 온다.
+                string fileFullName = ofd.FileName;
+                //File경로만 가지고 온다.
+                mPictureBox.Image = Image.FromFile(fileFullName);
+                string filePath = fileFullName.Replace(fileName, "");
+                timer3.Enabled = false;
+                y = 488;
+                a = 0;
+                num3 = 0;
+                n1 = 0;
+                b = 0;
+                ans2 = new int[400];
+                answerbox.Text = "";
+                timer1.Enabled = true;
+              
+                return fileFullName;
+            }
+            //취소버튼 클릭시 또는 ESC키로 파일창을 종료 했을경우
+            else if (dr == DialogResult.Cancel)
+            {
+                return "";
+            }
+
+            return "";
+        }
+     
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void compareanswer()
+        {
+         
+;
+            string[] str2 = answer.Text.Split(new string[] { "," }, StringSplitOptions.None);
+            string[] str3 = point_.Text.Split(new string[] { "," }, StringSplitOptions.None);
+   
+            if (checkBox1.Checked)
+            {
+                int right = 0;
+      
+                for (int i = 0; i < str2.Length; i++)
+                {
+                    double.TryParse(ans2[i].ToString(), out double aa);
+                    double.TryParse(str2[i], out double k);
+                    if (aa.Equals(k))
+                    {
+                        right++;
+                    }
+                }
+                if (str2.Length.Equals(right))
+                {
+                    finalpoint = 100;
+                }
+                else
+                {
+                    finalpoint = (100 / str2.Length) *right;
+                }
+               
+            }
+            else
+            {
+                if (str2.Length.Equals(str3.Length))
+                {
+                    for (int i = 0; i < str2.Length; i++)
+                    {
+                        double.TryParse(ans2[i].ToString(), out double aa);
+                        double.TryParse(str2[i], out double k);
+                        double.TryParse(str3[i], out double l);
+                        if (aa.Equals(k))
+                        {
+                            finalpoint += l;
+                        }
+                   
+
+                    }
+                }
+            }
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        int n1 = 0;
+        public int read_(ref Color col,int x)
+        {
+
+            if (col.GetBrightness()<=0.2)
+            {
+                if (x >= 1300 && x <= 1330 )
+                {
+                    n1 = 1;
+                }
+                else if (x >= 1350 && x <= 1370 )
+                {
+                    n1 = 2;
+                }
+                else if (x >= 1410 && x <= 1420)
+                {
+                    n1 = 3;
+                }
+                else if(x >= 1440 && x <= 1450 )
+                {
+                    n1 = 4;
+                }
+                else if(x >= 1490 && x <= 1510)
+                {
+                    n1 = 5;
+
+                }
+                else if(x < 1300 || x > 1510)
+                {
+                    n1 = 0;
+                }
+                else
+                {
+                    
+                }
+                
+                return n1;
+
+            }
+            else
+            {
+                if (n1 > 0)
+                {
+                    return n1;
+                }
+                else
+                {
+                    return 0;
+                }
+                
+            }
+
+        }
+        int y = 488;
+        int a = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap tmp = PictureBoxReset();
+
+
+            int width = tmp.Width;
+
+            int height = tmp.Height;
+            int x = 1331;
+            int n = 0;
+            Color colorData = new Color();
+
+      
+            if(num3<=20)
+
+            {
+
+                bool ischecked = false ;
+                for (x = 1300; x <= 1510; x++)
+
+                {
+                    colorData = tmp.GetPixel(x, y);
+                    n = read_(ref colorData, x);
+                    if (n > 0)
+                    {
+                        ischecked = true;
+                    }
+                   
+                }
+                if (ischecked)
+                {
+
+                    n1 = 0;
+                    y += 75;
+                    num3++;
+                    answerbox.Text += num3 + ":" + n + "\r\n";
+                    ans2[a] =n;
+                    a++;
+                }
+                if (num3.Equals(20))
+                {
+                    y = 488;
+                    x = 1600;
+                    for (x = 1600; x <= 1860; x++)
+
+                    {
+                        colorData = tmp.GetPixel(x, y);
+                        n = read__(ref colorData, x);
+                        if (n > 0)
+                        {
+                            ischecked = true;
+                        }
+
+                    }
+                    if (ischecked)
+                    {
+
+                        n1 = 0;
+                        y += 75;
+                        num3++;
+                        answerbox.Text += num3 + ":" + n + "\r\n";
+                        ans2[a] = n;
+                        a++;
+                    }
+
+                }
+
+            }
+            else if(num3>40)
+            {
+
+                timer1.Enabled = false;
+
+            }
+            else
+            {
+                bool ischecked = false;
+                for (x = 1600; x <= 1860; x++)
+
+                {
+                    colorData = tmp.GetPixel(x, y);
+                    n = read__(ref colorData, x);
+                    if (n > 0)
+                    {
+                        ischecked = true;
+                    }
+
+                }
+                if (ischecked)
+                {
+
+                    n1 = 0;
+                    y += 75;
+                    num3++;
+                    answerbox.Text += num3 + ":" + n + "\r\n";
+                    ans2[a] = n;
+                    a++;
+                }
+
+            }
+        }
+
+       
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            finalpoint = 0;
+            compareanswer();
+            result.Text =finalpoint.ToString();
+        }
+
+        private Bitmap PictureBoxReset()
+
+        {
+
+
+
+
+
+
+
+            Bitmap tmp = mPictureBox.Image as Bitmap;
+
+            //픽셀정보를 알아오기 위하여 비트맵을 얻어온다.         
+
+            return tmp;
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        public int read__(ref Color col, int x)
+        {
+
+            if (col.GetBrightness() <= 0.2)
+            {
+                if (x >= 1650 && x <= 1670)
+                {
+                    n1 = 1;
+                }
+                else if (x >= 1690 && x <= 1710)
+                {
+                    n1 = 2;
+                }
+                else if (x >= 1730 && x <= 1750)
+                {
+                    n1 = 3;
+                }
+                else if (x >= 1770 && x <= 1790)
+                {
+                    n1 = 4;
+                }
+                else if (x >= 1810 && x <= 1830)
+                {
+                    n1 = 5;
+
+                }
+                else if (x < 1630 || x > 1840)
+                {
+                    n1 = 0;
+                }
+                else
+                {
+
+                }
+
+                return n1;
+
+            }
+            else
+            {
+                if (n1 > 0)
+                {
+                    return n1;
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+
+        }
+        public int read0(ref Color col,int y)
+        {
+            if (col.GetBrightness() <= 0.2)
+            {
+            if(y>910 && y < 920)
+                {
+                    n1 = 0;
+                }
+            else if(y>950 && y < 960)
+                {
+                    n1 = 1;
+                }
+                else if (y > 1040 && y < 1050)
+                {
+                    n1 = 2;
+                }
+                else if (y > 1090 && y < 1100)
+                {
+                    n1 = 3;
+                }
+                else if (y > 1130 && y < 1140)
+                {
+                    n1 = 5;
+                }
+                else if (y > 1180 && y < 1190)
+                {
+                    n1 = 6;
+                }
+                else if (y > 1220 && y < 1230)
+                {
+                    n1 = 7;
+                }
+                else if (y > 1260 && y < 1270)
+                {
+                    n1 = 8;
+                }
+                else if (y > 1310 && y < 1320)
+                {
+                    n1 = 9;
+                }
+                return n1;
+
+            }
+            else
+            {
+                if (n1 > 0)
+                {
+                    return n1;
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+        }
+    }
+    
+}
+
